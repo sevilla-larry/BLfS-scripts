d6584ccb1fdb3f8da2ae72a9f40d443dba05f0a9
Sevilla, Larry <sevilla.larry.oss@gmail.com>
2024-03-27 22:09:12 +0800

    MariaDB update 1

--- 12.1/c11.MariaDB/c11.22.03.MariaDB-10.11.7.sh
+++ 12.1/c11.MariaDB/c11.22.03.MariaDB-10.11.7.sh
@@ -30,8 +30,8 @@ export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
 export PKGLOG_CHECK=$PKGLOG_DIR/check.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
-export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
 export LFSLOG_PROCESS=$LFSLOG/process.log
 
 rm -r $PKGLOG_DIR 2> /dev/null
@@ -228,6 +228,7 @@ cd ..
 cd ..
 rm -rf $PKG
 unset LFSLOG_PROCESS
+unset PKGLOG_OTHERS
 unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
 unset PKGLOG_CHECK
 unset PKGLOG_ERROR PKGLOG_TAR
