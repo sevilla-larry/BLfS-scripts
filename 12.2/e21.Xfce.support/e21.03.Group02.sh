9f51521171d16a03e6d4187ed3e8e06d214f8c94
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-10-08 12:02:19 +0800

    libxkbcommon need by GTK+ 3

--- 12.2/e21.Xfce.support/e21.03.Group02.sh
+++ 12.2/e21.Xfce.support/e21.03.Group02.sh
@@ -19,6 +19,19 @@ echo "."
 
 ###
 
+echo "e21.09.71.libxkbcommon-1.7.0"
+echo "e21.09.71.libxkbcommon-1.7.0" >> $LFSLOG_PROCESS
+
+time { bash e21.09.71.libxkbcommon-1.7.0.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
 echo "e21.10.06.FriBidi-1.0.15"
 echo "e21.10.06.FriBidi-1.0.15" >> $LFSLOG_PROCESS
 
