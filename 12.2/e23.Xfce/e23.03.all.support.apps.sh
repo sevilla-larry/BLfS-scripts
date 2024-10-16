fb778fded9106732b9ea3b68d79f9aab57ee2f0e
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-10-16 15:46:13 +0800

    delete group 4

--- 12.2/e23.Xfce/e23.03.all.support.apps.sh
+++ 12.2/e23.Xfce/e23.03.all.support.apps.sh
@@ -3,10 +3,9 @@
 # Xfce Support Applications
 #
 
- 
+bash e10.03.Group01.sh
 bash e11.03.Group01.sh
 
 bash e21.03.Group01.sh
 bash e21.03.Group02.sh
 bash e21.03.Group03.sh
-bash e21.03.Group04.sh
