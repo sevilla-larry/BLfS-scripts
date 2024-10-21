8e0dd2ec0dc0e45a432bd8545655b646147eb6e0
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-10-21 20:07:52 +0800

    xdg desktop portal

---
+++ 12.2/e10.common.DE/e10.25.48.xdg-desktop-portal-1.18.2.sh
@@ -0,0 +1,82 @@
+# e10.25.48.xdg-desktop-portal-1.18.2.sh
+#
+
+#
+# Dependencies Required:
+#
+#               e10.05.04 Fuse-3.16.2           ???
+#               e10.25.10 gdk-pixbuf-2.42.12
+#               e10.09.29 JSON-Glib-1.8.0
+#               e10.42.48 Pipewire-1.2.3        ???
+#
+# Needed at runtime:
+#
+#               ???.??.?? xdg-desktop-portal-{gtk,gnome,lxqt}
+#
+
+#
+# Required (at runtime) by:
+#
+#               e33.37.34 xdg-desktop-portal-lxqt-1.0.2
+#
+
+export PKG="xdg-desktop-portal-1.18.2"
+export PKGLOG_DIR=$LFSLOG/25.48
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $LFSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.xz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+mkdir build 
+cd    build 
+
+echo "2. Meson Setup ..."
+echo "2. Meson Setup ..." >> $LFSLOG_PROCESS
+echo "2. Meson Setup ..." >> $PKGLOG_ERROR
+meson setup --prefix=/usr       \
+            --buildtype=release \
+            ..                  \
+        > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+
+echo "3. Ninja Build ..."
+echo "3. Ninja Build ..." >> $LFSLOG_PROCESS
+echo "3. Ninja Build ..." >> $PKGLOG_ERROR
+ninja > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "4. Ninja Test ..."
+echo "4. Ninja Test ..." >> $LFSLOG_PROCESS
+echo "4. Ninja Test ..." >> $PKGLOG_ERROR
+ninja test > $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+echo "5. Ninja Install ..."
+echo "5. Ninja Install ..." >> $LFSLOG_PROCESS
+echo "5. Ninja Install ..." >> $PKGLOG_ERROR
+ninja install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+rm -rvf /usr/lib/systemd    \
+    > $PKGLOG_OTHERS 2>> $PKGLOG_ERROR
+
+
+cd ..
+cd ..
+rm -rf $PKG
+unset LFSLOG_PROCESS
+unset PKGLOG_OTHERS
+unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
