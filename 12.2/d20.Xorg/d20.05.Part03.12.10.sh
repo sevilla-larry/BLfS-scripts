e9dcdb2de29668e57c916064a1ccc08cd2939ba2
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-10-22 16:43:24 +0800

    bug fix

--- 12.2/d20.Xorg/d20.05.Part03.12.10.sh
+++ 12.2/d20.Xorg/d20.05.Part03.12.10.sh
@@ -10,10 +10,10 @@ date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
 #####
 
-echo "d10.12.10.dbus-1.14.10"
-echo "d10.12.10.dbus-1.14.10" >> $LFSLOG_PROCESS
+echo "d20.12.10.dbus-1.14.10"
+echo "d20.12.10.dbus-1.14.10" >> $LFSLOG_PROCESS
 
-time { bash d10.12.10.dbus-1.14.10.sh ; }
+time { bash d20.12.10.dbus-1.14.10.sh ; }
 
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
