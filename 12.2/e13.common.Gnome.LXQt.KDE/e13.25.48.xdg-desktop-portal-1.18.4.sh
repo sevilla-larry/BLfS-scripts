b710dcb6d6c791d10eddcaab5a3d43ecdddbf6cf
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-10-24 07:58:08 +0800

    errata update of xdg-desktop-portal svn update of JSON-Glib

--- 12.2/e13.common.Gnome.LXQt.KDE/e13.25.48.xdg-desktop-portal-1.18.4.sh
+++ 12.2/e13.common.Gnome.LXQt.KDE/e13.25.48.xdg-desktop-portal-1.18.4.sh
@@ -1,26 +1,39 @@
-# e10.09.29.JSON-GLib-1.8.0.sh
+# e10.25.48.xdg-desktop-portal-1.18.4.sh
+# errata
 #
 
 #
 # Dependencies Required:
 #
-#               d10.09.17 GLib-2.80.4
+#               e10.05.04 Fuse-3.16.2           ???
+#               e10.25.10 gdk-pixbuf-2.42.12
+#               e10.09.29 JSON-Glib-1.10.0      svn due to errata
+#               e10.42.48 Pipewire-1.2.3        ???
+#
+# Dependencies Required (at runtime):
+#
+#               d20.12.10 dbus-1.14.10
+#
+# Needed at runtime:
+#
+#               ???.??.?? xdg-desktop-portal-{gtk,gnome,lxqt}
 #
 
 #
-# Required by:
+# Required (at runtime) by:
 #
-#               e10.25.48 xdg-desktop-portal-1.18.2
+#               e33.37.34 xdg-desktop-portal-lxqt-1.0.2
 #
 
-export PKG="json-glib-1.8.0"
-export PKGLOG_DIR=$LFSLOG/09.29
+export PKG="xdg-desktop-portal-1.18.4"
+export PKGLOG_DIR=$LFSLOG/25.48
 export PKGLOG_TAR=$PKGLOG_DIR/tar.log
 export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
 export PKGLOG_CHECK=$PKGLOG_DIR/check.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
 export LFSLOG_PROCESS=$LFSLOG/process.log
 
 rm -r $PKGLOG_DIR 2> /dev/null
@@ -59,11 +72,15 @@ echo "5. Ninja Install ..." >> $LFSLOG_PROCESS
 echo "5. Ninja Install ..." >> $PKGLOG_ERROR
 ninja install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
+rm -rvf /usr/lib/systemd    \
+    > $PKGLOG_OTHERS 2>> $PKGLOG_ERROR
+
 
 cd ..
 cd ..
 rm -rf $PKG
 unset LFSLOG_PROCESS
+unset PKGLOG_OTHERS
 unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
 unset PKGLOG_CHECK
 unset PKGLOG_ERROR PKGLOG_TAR
