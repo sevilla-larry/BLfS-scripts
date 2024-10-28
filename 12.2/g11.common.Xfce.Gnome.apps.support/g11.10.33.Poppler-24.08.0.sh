ab802da35fc0e15b54148c6c34198b535baa7761
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-10-28 10:52:34 +0800

    bug fix

--- 12.2/g11.common.Xfce.Gnome.apps.support/g11.10.33.Poppler-24.08.0.sh
+++ 12.2/g11.common.Xfce.Gnome.apps.support/g11.10.33.Poppler-24.08.0.sh
@@ -43,24 +43,24 @@ cmake -D CMAKE_BUILD_TYPE=Release           \
       ..                                    \
       > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
 
-echo "3.1 Git some Test ..."
-echo "3.1 Git some Test ..." >> $LFSLOG_PROCESS
-echo "3.1 Git some Test ..." >> $PKGLOG_ERROR
+echo "3. Ninja Build ..."
+echo "3. Ninja Build ..." >> $LFSLOG_PROCESS
+echo "3. Ninja Build ..." >> $PKGLOG_ERROR 
+ninja > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "4.1 Git some Test ..."
+echo "4.1 Git some Test ..." >> $LFSLOG_PROCESS
+echo "4.1 Git some Test ..." >> $PKGLOG_ERROR
  git clone --depth 1    \
         https://gitlab.freedesktop.org/poppler/test.git \
         testfiles       \
       > $PKGLOG_OTHERS 2>> $PKGLOG_ERROR
 
-echo "3.2 Ninja Test ..."
-echo "3.2 Ninja Test ..." >> $LFSLOG_PROCESS
-echo "3.2 Ninja Test ..." >> $PKGLOG_ERROR
+echo "4.2 Ninja Test ..."
+echo "4.2 Ninja Test ..." >> $LFSLOG_PROCESS
+echo "4.2 Ninja Test ..." >> $PKGLOG_ERROR
 LC_ALL=en_US.UTF-8 ninja test   \
       > $PKGLOG_CHECK 2>> $PKGLOG_ERROR
-
-echo "4. Ninja Build ..."
-echo "4. Ninja Build ..." >> $LFSLOG_PROCESS
-echo "4. Ninja Build ..." >> $PKGLOG_ERROR 
-ninja > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
 
 echo "5. Ninja Install ..."
 echo "5. Ninja Install ..." >> $LFSLOG_PROCESS
