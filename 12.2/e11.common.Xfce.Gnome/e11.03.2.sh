24ce1178f4a5f37016413032a73258198ed1588c
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-04 20:03:11 +0800

    gst

--- 12.2/e11.common.Xfce.Gnome/e11.03.2.sh
+++ 12.2/e11.common.Xfce.Gnome/e11.03.2.sh
@@ -10,59 +10,59 @@ date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
 #####
 
-# echo "e11.42.14.gstreamer-1.24.7"
-# echo "e11.42.14.gstreamer-1.24.7" >> $LFSLOG_PROCESS
+echo "e11.42.14.gstreamer-1.24.7"
+echo "e11.42.14.gstreamer-1.24.7" >> $LFSLOG_PROCESS
 
-# time { bash e11.42.14.gstreamer-1.24.7.sh ; }
+time { bash e11.42.14.gstreamer-1.24.7.sh ; }
 
-# date +"%Y/%b/%d %H:%M"
-# date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
-# echo "." >> $LFSLOG_PROCESS
-# echo "."
-# echo "."
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
 
-# ###
+###
 
-# echo "e11.42.15.gst-plugins-base-1.24.7"
-# echo "e11.42.15.gst-plugins-base-1.24.7" >> $LFSLOG_PROCESS
+echo "e11.42.15.gst-plugins-base-1.24.7"
+echo "e11.42.15.gst-plugins-base-1.24.7" >> $LFSLOG_PROCESS
 
-# time { bash e11.42.15.gst-plugins-base-1.24.7.sh ; }
+time { bash e11.42.15.gst-plugins-base-1.24.7.sh ; }
 
-# date +"%Y/%b/%d %H:%M"
-# date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
-# echo "." >> $LFSLOG_PROCESS
-# echo "."
-# echo "."
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
 
-# ###
+###
 
-# echo "e11.42.16.gst-plugins-good-1.24.7"
-# echo "e11.42.16.gst-plugins-good-1.24.7" >> $LFSLOG_PROCESS
+echo "e11.42.16.gst-plugins-good-1.24.7"
+echo "e11.42.16.gst-plugins-good-1.24.7" >> $LFSLOG_PROCESS
 
-# time { bash e11.42.16.gst-plugins-good-1.24.7.sh ; }
+time { bash e11.42.16.gst-plugins-good-1.24.7.sh ; }
 
-# date +"%Y/%b/%d %H:%M"
-# date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
-# echo "." >> $LFSLOG_PROCESS
-# echo "."
-# echo "."
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
 
-# ###
+###
 
-# echo "e11.42.17.gst-plugins-bad-1.24.7"
-# echo "e11.42.17.gst-plugins-bad-1.24.7" >> $LFSLOG_PROCESS
+echo "e11.42.17.gst-plugins-bad-1.24.7"
+echo "e11.42.17.gst-plugins-bad-1.24.7" >> $LFSLOG_PROCESS
 
-# time { bash e11.42.17.gst-plugins-bad-1.24.7.sh ; }
+time { bash e11.42.17.gst-plugins-bad-1.24.7.sh ; }
 
-# date +"%Y/%b/%d %H:%M"
-# date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
-# echo "." >> $LFSLOG_PROCESS
-# echo "."
-# echo "."
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
 
 #####
 
