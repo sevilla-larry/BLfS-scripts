31bbdd981edc149666c6802992dd0817ab163844
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-05 16:14:04 +0800

    poppler support apps

--- 12.2/g11.common.Xfce.Gnome.apps.support/g11.03.3.sh
+++ 12.2/g11.common.Xfce.Gnome.apps.support/g11.03.3.sh
@@ -112,36 +112,6 @@ echo "." >> $LFSLOG_PROCESS
 echo "."
 echo "."
 
-###
-#	NSS group
-###
-
-echo "g11.09.82.NSPR-4.36"
-echo "g11.09.82.NSPR-4.36" >> $LFSLOG_PROCESS
-
-time { bash g11.09.82.NSPR-4.36.sh ; }
-
-date +"%Y/%b/%d %H:%M"
-date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
-
-echo "." >> $LFSLOG_PROCESS
-echo "."
-echo "."
-
-###
-
-echo "g11.04.17.NSS-3.103"
-echo "g11.04.17.NSS-3.103" >> $LFSLOG_PROCESS
-
-time { bash g11.04.17.NSS-3.103.sh ; }
-
-date +"%Y/%b/%d %H:%M"
-date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
-
-echo "." >> $LFSLOG_PROCESS
-echo "."
-echo "."
-
 #####
 
 unset LFSLOG_PROCESS
