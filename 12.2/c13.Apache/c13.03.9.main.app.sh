35d2c5d29769c88f8a530568aeae23d841026947
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-25 13:20:24 +0800

    description

--- 12.2/c13.Apache/c13.03.9.main.app.sh
+++ 12.2/c13.Apache/c13.03.9.main.app.sh
@@ -10,8 +10,8 @@ date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
 #####
 
-echo "20.01.Apache-2.4.62"
-echo "20.01.Apache-2.4.62" >> $LFSLOG_PROCESS
+echo "c13.20.01.Apache-2.4.62"
+echo "c13.20.01.Apache-2.4.62" >> $LFSLOG_PROCESS
 
 time { bash c13.20.01.Apache-2.4.62.sh ; }
 
