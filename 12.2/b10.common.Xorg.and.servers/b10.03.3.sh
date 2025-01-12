5b399e1bc8470fe2350da9c52ad37972de39236d
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-01-12 20:06:32 +0800

    move sqlite to LfS before nss, for firefox

--- 12.2/b10.common.Xorg.and.servers/b10.03.3.sh
+++ 12.2/b10.common.Xorg.and.servers/b10.03.3.sh
@@ -11,19 +11,19 @@ export LFSLOG_PROCESS=$LFSLOG/process.log
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
-#####
+##### to LfS
 
-echo "b10.22.05.SQLite-3.46.1"
-echo "b10.22.05.SQLite-3.46.1" >> $LFSLOG_PROCESS
+# echo "b10.22.05.SQLite-3.46.1"
+# echo "b10.22.05.SQLite-3.46.1" >> $LFSLOG_PROCESS
+# 
+# time { bash b10.22.05.SQLite-3.46.1.sh ; }
+# 
+# date +"%Y/%b/%d %H:%M"
+# date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
-time { bash b10.22.05.SQLite-3.46.1.sh ; }
-
-date +"%Y/%b/%d %H:%M"
-date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
-
-echo "." >> $LFSLOG_PROCESS
-echo "."
-echo "."
+# echo "." >> $LFSLOG_PROCESS
+# echo "."
+# echo "."
 
 #####
 
