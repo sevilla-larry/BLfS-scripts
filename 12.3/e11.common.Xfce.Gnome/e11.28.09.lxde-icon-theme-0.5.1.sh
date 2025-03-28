9424992bb8dd3b18d1fb6e62878751420cfd3869
freshel <freshelellena@gmail.com>
2025-03-28 16:48:36 +0800

    upload 12.3 E11

---
+++ 12.3/e11.common.Xfce.Gnome/e11.28.09.lxde-icon-theme-0.5.1.sh
@@ -0,0 +1,66 @@
+# e21.28.09.lxde-icon-theme-0.5.1.sh
+#
+
+#
+# Dependencies Optional:
+#
+#               e21.25.17 GTK+-3.24.43
+#
+
+#
+# Required by:
+#
+#               ??? 37.10 lxde-common-0.99.2    ???
+#
+# Recommended by:
+#
+#               ??? 37.05 PCManFM-1.3.2         ???
+#
+# Runtime needed by:
+#
+#               e23.35.13 xfce4-settings-4.18.6
+#
+
+export PKG="lxde-icon-theme-0.5.1"
+export PKGLOG_DIR=$LFSLOG/28.09
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+#export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
+export SOURCES= `pwd`
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $LFSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.xz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+echo "2. Configure ..."
+echo "2. Configure ..." >> $LFSLOG_PROCESS
+echo "2. Configure ..." >> $PKGLOG_ERROR
+./configure --prefix=/usr       \
+            > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+
+echo "3. Make Install ..."
+echo "3. Make Install ..." >> $LFSLOG_PROCESS
+echo "3. Make Install ..." >> $PKGLOG_ERROR
+make install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+gtk-update-icon-cache -qf /usr/share/icons/nuoveXT2
+
+
+cd $SOURCES
+rm -rf $PKG
+unset SOURCES
+unset LFSLOG_PROCESS
+unset PKGLOG_INSTALL
+# PKGLOG_BUILD
+unset PKGLOG_CONFIG
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
---
+++ 12.3/e11.common.Xfce.Gnome/e11.33.07.VTE-0.78.4.sh
@@ -0,0 +1,86 @@
+# e11.33.07.VTE-0.78.4.sh
+#
+
+#
+# Dependencies Required:
+#
+#               e11.25.17 GTK+-3.24.43
+#               b10.09.72 libxml2-2.13.3
+#               b15.09.83 PCRE2-10.44
+#
+# Dependencies Recommended:
+#
+#               b10.09.24 icu-75.1
+#               d10.09.17 GLib-2.80.4
+#               e11.04.07 GnuTLS-3.8.7.1
+#
+
+#
+# Required Runtime by:
+#
+#               g62.36.02 xfce4-terminal-1.1.3
+#
+
+export PKG="vte-0.78.4"
+export PKGLOG_DIR=$LFSLOG/33.07
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
+export SOURCES= `pwd`
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $LFSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+ 
+
+mkdir build
+cd    build
+
+echo "2. Meson Setup ..."
+echo "2. Meson Setup ..." >> $LFSLOG_PROCESS
+echo "2. Meson Setup ..." >> $PKGLOG_ERROR
+meson setup --prefix=/usr       \
+            --buildtype=release \
+            -D _systemd=false   \
+            -D gtk4=false       \
+    > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+#            -D gnutls=false     \
+
+echo "3. Ninja Build ..."
+echo "3. Ninja Build ..." >> $LFSLOG_PROCESS
+echo "3. Ninja Build ..." >> $PKGLOG_ERROR 
+ninja > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "4. Ninja Test ..."
+echo "4. Ninja Test ..." >> $LFSLOG_PROCESS
+echo "4. Ninja Test ..." >> $PKGLOG_ERROR
+ninja test > $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+echo "5. Ninja Install ..."
+echo "5. Ninja Install ..." >> $LFSLOG_PROCESS
+echo "5. Ninja Install ..." >> $PKGLOG_ERROR
+ninja install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+rm -v /etc/profile.d/vte.*  \
+    > $PKGLOG_OTHERS 2>> $PKGLOG_ERROR
+
+
+cd $SOURCES
+rm -rf $PKG
+unset SOURCES
+unset LFSLOG_PROCESS
+unset PKGLOG_OTHERS
+unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
