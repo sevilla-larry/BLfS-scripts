1c4ff2a162dbf366b20b497be8f54ffe89a22738
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-05-02 20:38:14 +0800

    d20 update

--- 12.3/d20.Xorg/d20.03.9.Group2.sh
+++ 12.3/d20.Xorg/d20.03.9.Group2.sh
@@ -5,29 +5,30 @@
 # Part 04:	09 & 10
 # Part 05:          XCB Utilities
 # Part 06:	25.33
-# Part 07:	42.41	for PC
+# Part 07:	42.41	sub 1 for PC
 # Part 08:	12.Mesa
-# Part 09:	42.xx	for PC
+# Part 09:	42.xx	sub 2 for PC
 # Part 10:	13
 # Part 11:	14	Xorg Applications
 # Part 12:	15 ---> 16
 # Part 13:	17	Xorg Fonts
 # Part 14:	25.34
+# Part 15:  09.71
 # Part 16:	18 ---> 28
 #
 
 bash d20.03.1.Part04.09-10.sh
-bash d20.03.1.Part05.sh
+bash d20.03.1.Part05.11.sh
 bash d20.03.1.Part06.25.33.sh
 
 if	[ "$COMPUTER_TYPE" -eq "PC" ]; then
-	bash d20.03.1.Part07.42.3.PC.sh
+	bash d20.03.1.Part07.42-1.PC.sh
 fi
 
 bash d20.03.1.Part08.12.sh
 
 if	[ "$COMPUTER_TYPE" -eq "PC" ]; then
-	bash d20.03.1.Part09.42.3.PC.sh
+	bash d20.03.1.Part09.42-2.PC.sh
 fi
 
 bash d20.03.1.Part10.13.sh
