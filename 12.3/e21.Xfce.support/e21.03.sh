65a3a081242acd149c771276cc3488556ab11817
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-05-19 19:42:18 +0800

    add libdisplay

--- 12.3/e21.Xfce.support/e21.03.sh
+++ 12.3/e21.Xfce.support/e21.03.sh
@@ -5,6 +5,20 @@ export LFSLOG_PROCESS=$LFSLOG/process.log
 
 #####
 
+echo "e21.09.41.libdisplay-info-0.2.0"
+echo "e21.09.41.libdisplay-info-0.2.0" >> $LFSLOG_PROCESS
+
+time { bash e21.09.41.libdisplay-info-0.2.0.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
 echo "e21.25.36.libnotify-0.8.4"
 echo "e21.25.36.libnotify-0.8.4" >> $LFSLOG_PROCESS
 
