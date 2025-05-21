3d4ae1d9850ff0435850e699d43126ae57154b30
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-05-22 07:14:20 +0800

    move E13 files to E11 for the meantime

--- 12.3/e13.common.Xfce.Gnome.KDE/e13.03.sh
+++ 12.3/e13.common.Xfce.Gnome.KDE/e13.03.sh
@@ -8,18 +8,6 @@ date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
 
 #####
 
-echo "e13.12.02.AccountsService-23.13.9"
-echo "e13.12.02.AccountsService-23.13.9" >> $LFSLOG_PROCESS
-
-time { bash e13.12.02.AccountsService-23.13.9.sh ; }
-
-date +"%Y/%b/%d %H:%M"
-date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
-
-echo "." >> $LFSLOG_PROCESS
-echo "."
-echo "."
-
 #####
 
 unset LFSLOG_PROCESS
