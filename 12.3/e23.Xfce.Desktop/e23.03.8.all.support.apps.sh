ca83745ad2cd3d373e57625122f86b96f4fe8086
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-05-23 09:10:15 +0800

    E11 fixes addl

--- 12.3/e23.Xfce.Desktop/e23.03.8.all.support.apps.sh
+++ 12.3/e23.Xfce.Desktop/e23.03.8.all.support.apps.sh
@@ -6,17 +6,13 @@
 # common apps to all Desktop Environments
 bash e10.03.1.sh
 bash e10.03.2.sh
-
 #if	 [ "$WITH_SOUND" -eq 1 ]; then
 #fi
 
 # common apps to Xfce & Gnome
 bash e11.03.1.sh
 bash e11.03.2.sh
-bash e11.03.3.sh
-
 #if	 [ "$WITH_SOUND" -eq 1 ]; then
-    bash e11.03.4.sh
 #fi
 
 # Xfce support apps
