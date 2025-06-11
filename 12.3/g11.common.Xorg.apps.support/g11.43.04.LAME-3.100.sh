3a51972860988b800686c7d6856059d98451306d
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-06-11 20:14:55 +0800

    Add lame & mpg123

---
+++ 12.3/g11.common.Xorg.apps.support/g11.43.04.LAME-3.100.sh
@@ -0,0 +1,77 @@
+# g11.43.04.LAME-3.100.sh
+#
+
+#
+# Dependencies Optional:
+#
+#           ??? g11.42.40 libsndfile-1.2.2  circular
+#               e10.13.17 NASM-2.16.03
+#
+
+#
+# Optionally by:
+#
+#               g11.42.40 libsndfile-1.2.2
+#
+
+export PKG="lame-3.100"
+export PKGLOG_DIR=$LFSLOG/43.04
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
+export SOURCES=`pwd`
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $LFSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+sed -i -e 's/^\(\s*hardcode_libdir_flag_spec\s*=\).*/\1/'   \
+            configure \
+            > $PKGLOG_OTHERS 2>> $PKGLOG_ERROR
+
+echo "2. Configure ..."
+echo "2. Configure ..." >> $LFSLOG_PROCESS
+echo "2. Configure ..." >> $PKGLOG_ERROR
+../configure --prefix=/usr      \
+             --enable-mp3rtp    \
+             --disable-static   \
+             > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+
+echo "3. Make Build ..."
+echo "3. Make Build ..." >> $LFSLOG_PROCESS
+echo "3. Make Build ..." >> $PKGLOG_ERROR
+make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "4. Make Test ..."
+echo "4. Make Test ..." >> $LFSLOG_PROCESS
+echo "4. Make Test ..." >> $PKGLOG_ERROR
+LD_LIBRARY_PATH=libmp3lame/.libs make test  \
+            > $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+echo "5. Make Install ..."
+echo "5. Make Install ..." >> $LFSLOG_PROCESS
+echo "5. Make Install ..." >> $PKGLOG_ERROR
+make pkghtmldir=/usr/share/doc/lame-3.100 install   \
+            > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+
+cd $SOURCES
+rm -rf $PKG
+unset SOURCES
+unset LFSLOG_PROCESS
+unset PKGLOG_OTHERS
+unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
--- 12.3/g11.common.Xorg.apps.support/g11.43.05.mpg123-1.32.10.sh
+++ 12.3/g11.common.Xorg.apps.support/g11.43.05.mpg123-1.32.10.sh
@@ -1,21 +1,31 @@
-# e11.42.53.sound-theme-freedesktop-0.8.sh
+# g11.43.05.mpg123-1.32.10.sh
 #
 
 #
-# Recommended ( Run Time ) by:
+# Dependencies Recommended:
 #
-#               e11.42.26 libcanbera-0.30
+#               e10.42.01 alsa-lib-1.2.13
+#
+# Dependencies Optional:
+#
+#           ??? g11.42.49 PulseAudio-17.0   circular
+#           ??? g11.42.52 SDL2-2.30.11      circular
 #
 
-export PKG="sound-theme-freedesktop-0.8"
-export PKGLOG_DIR=$LFSLOG/42.53
+#
+# Optionally by:
+#
+#               g11.42.40 libsndfile-1.2.2
+#
+
+export PKG="mpg123-1.32.10"
+export PKGLOG_DIR=$LFSLOG/43.05
 export PKGLOG_TAR=$PKGLOG_DIR/tar.log
 export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
-#export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
-#export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
 export LFSLOG_PROCESS=$LFSLOG/process.log
 export SOURCES=`pwd`
 
@@ -32,17 +42,22 @@ cd $PKG
 echo "2. Configure ..."
 echo "2. Configure ..." >> $LFSLOG_PROCESS
 echo "2. Configure ..." >> $PKGLOG_ERROR
-./configure --prefix=/usr   \
-        > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+./configure --prefix=/usr    \
+            > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
 
 echo "3. Make Build ..."
 echo "3. Make Build ..." >> $LFSLOG_PROCESS
 echo "3. Make Build ..." >> $PKGLOG_ERROR
 make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
 
-echo "4. Make Install ..."
-echo "4. Make Install ..." >> $LFSLOG_PROCESS
-echo "4. Make Install ..." >> $PKGLOG_ERROR
+echo "4. Make Check ..."
+echo "4. Make Check ..." >> $LFSLOG_PROCESS
+echo "4. Make Check ..." >> $PKGLOG_ERROR
+make check > $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+echo "5. Make Install ..."
+echo "5. Make Install ..." >> $LFSLOG_PROCESS
+echo "5. Make Install ..." >> $PKGLOG_ERROR
 make install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
 
@@ -50,8 +65,7 @@ cd $SOURCES
 rm -rf $PKG
 unset SOURCES
 unset LFSLOG_PROCESS
-#unset PKGLOG_OTHERS
 unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
-#unset PKGLOG_CHECK
+unset PKGLOG_CHECK
 unset PKGLOG_ERROR PKGLOG_TAR
 unset PKGLOG_DIR PKG
