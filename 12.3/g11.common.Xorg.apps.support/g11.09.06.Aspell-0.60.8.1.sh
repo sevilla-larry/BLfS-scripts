af81653424e73257c5e141a6fdd5f074e652266b
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-06-13 09:59:35 +0800

    add separators in log

--- 12.3/g11.common.Xorg.apps.support/g11.09.06.Aspell-0.60.8.1.sh
+++ 12.3/g11.common.Xorg.apps.support/g11.09.06.Aspell-0.60.8.1.sh
@@ -34,20 +34,20 @@ tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
 cd $PKG
 
 
-echo "2. Configure ..."
-echo "2. Configure ..." >> $LFSLOG_PROCESS
-echo "2. Configure ..." >> $PKGLOG_ERROR
+echo "2. Configure main ..."
+echo "2. Configure main ..." >> $LFSLOG_PROCESS
+echo "2. Configure main ..." >> $PKGLOG_ERROR
 ./configure --prefix=/usr   \
           > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
 
-echo "3. Make Build ..."
-echo "3. Make Build ..." >> $LFSLOG_PROCESS
-echo "3. Make Build ..." >> $PKGLOG_ERROR
+echo "3. Make Build main ..."
+echo "3. Make Build main ..." >> $LFSLOG_PROCESS
+echo "3. Make Build main ..." >> $PKGLOG_ERROR
 make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
 
-echo "4. Make Install ..."
-echo "4. Make Install ..." >> $LFSLOG_PROCESS
-echo "4. Make Install ..." >> $PKGLOG_ERROR
+echo "4. Make Install main ..."
+echo "4. Make Install main ..." >> $LFSLOG_PROCESS
+echo "4. Make Install main ..." >> $PKGLOG_ERROR
 make install > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
 ln -svfn aspell-0.60 /usr/lib/aspell                \
@@ -71,6 +71,26 @@ install -v -m 755 scripts/spell     /usr/bin/       \
         >> $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
 # Setup Dictionary
+
+echo ".........." >> $PKGLOG_ERROR
+echo "...Dict..." >> $PKGLOG_ERROR
+echo ".........." >> $PKGLOG_ERROR
+
+echo ".........." >> $PKGLOG_TAR
+echo "...Dict..." >> $PKGLOG_TAR
+echo ".........." >> $PKGLOG_TAR
+
+echo ".........." >> $PKGLOG_CONFIG
+echo "...Dict..." >> $PKGLOG_CONFIG
+echo ".........." >> $PKGLOG_CONFIG
+
+echo ".........." >> $PKGLOG_BUILD
+echo "...Dict..." >> $PKGLOG_BUILD
+echo ".........." >> $PKGLOG_BUILD
+
+echo ".........." >> $PKGLOG_INSTALL
+echo "...Dict..." >> $PKGLOG_INSTALL
+echo ".........." >> $PKGLOG_INSTALL
 
 echo "5. Extract Dictionary tar..."
 echo "5. Extract Dictionary tar..." >> $LFSLOG_PROCESS
