e4bd9fbe277846991287260aa22546bc50145d64
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-06-22 19:59:38 +0800

    E42 & E43 bash sequence

---
+++ 12.3/e42.Gnome.Libraries/e42.03.sh
@@ -0,0 +1,367 @@
+# e42.03.sh
+#
+# Gnome Libraries
+#
+
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+#####
+
+echo "e42.33.04.libsecret-0.21.6"
+echo "e42.33.04.libsecret-0.21.6" >> $LFSLOG_PROCESS
+
+time { bash e42.33.04.libsecret-0.21.6.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.01.Gcr-3.41.2"
+echo "e42.33.01.Gcr-3.41.2" >> $LFSLOG_PROCESS
+
+time { bash e42.33.01.Gcr-3.41.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.02.Gcr-4.3.1"
+echo "e42.33.02.Gcr-4.3.1" >> $LFSLOG_PROCESS
+
+time { bash e42.33.02.Gcr-4.3.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+#
+#	e11.33.03.gsettings-desktop-schemas-47.1
+#
+#	build ahead:
+#		e42.33.04.libsecret-0.21.6
+#
+###
+
+echo "e42.33.05.rest-0.9.1"
+echo "e42.33.05.rest-0.9.1" >> $LFSLOG_PROCESS
+
+time { bash e42.33.05.rest-0.9.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.06.totem-pl-parser-3.26.6"
+echo "e42.33.06.totem-pl-parser-3.26.6" >> $LFSLOG_PROCESS
+
+time { bash e42.33.06.totem-pl-parser-3.26.6.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.07.VTE-0.78.4"
+echo "e42.33.07.VTE-0.78.4" >> $LFSLOG_PROCESS
+
+time { bash e42.33.07.VTE-0.78.4.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.08.yelp-xsl-42.1"
+echo "e42.33.08.yelp-xsl-42.1" >> $LFSLOG_PROCESS
+
+time { bash e42.33.08.yelp-xsl-42.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.09.geocode-glib-3.26.4"
+echo "e42.33.09.geocode-glib-3.26.4" >> $LFSLOG_PROCESS
+
+time { bash e42.33.09.geocode-glib-3.26.4.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.10.Gjs-1.82.1"
+echo "e42.33.10.Gjs-1.82.1" >> $LFSLOG_PROCESS
+
+time { bash e42.33.10.Gjs-1.82.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.11.gnome-autoar-0.4.5"
+echo "e42.33.11.gnome-autoar-0.4.5" >> $LFSLOG_PROCESS
+
+time { bash e42.33.11.gnome-autoar-0.4.5.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.12.gnome-desktop-44.1"
+echo "e42.33.12.gnome-desktop-44.1" >> $LFSLOG_PROCESS
+
+time { bash e42.33.12.gnome-desktop-44.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.13.gnome-menus-3.36.0"
+echo "e42.33.13.gnome-menus-3.36.0" >> $LFSLOG_PROCESS
+
+time { bash e42.33.13.gnome-menus-3.36.0.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.14.gnome-online-accounts-3.52.3.1"
+echo "e42.33.14.gnome-online-accounts-3.52.3.1" >> $LFSLOG_PROCESS
+
+time { bash e42.33.14.gnome-online-accounts-3.52.3.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.15.libgdata-0.18.1"
+echo "e42.33.15.libgdata-0.18.1" >> $LFSLOG_PROCESS
+
+time { bash e42.33.15.libgdata-0.18.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.16.libgee-0.20.8"
+echo "e42.33.16.libgee-0.20.8" >> $LFSLOG_PROCESS
+
+time { bash e42.33.16.libgee-0.20.8.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.17.libgtop-2.41.3"
+echo "e42.33.17.libgtop-2.41.3" >> $LFSLOG_PROCESS
+
+time { bash e42.33.17.libgtop-2.41.3.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.18.libgweather-4.4.4"
+echo "e42.33.18.libgweather-4.4.4" >> $LFSLOG_PROCESS
+
+time { bash e42.33.18.libgweather-4.4.4.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.19.libpeas-1.36.0"
+echo "e42.33.19.libpeas-1.36.0" >> $LFSLOG_PROCESS
+
+time { bash e42.33.19.libpeas-1.36.0.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.20.libshumate-1.3.2"
+echo "e42.33.20.libshumate-1.3.2" >> $LFSLOG_PROCESS
+
+time { bash e42.33.20.libshumate-1.3.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.21.evolution-data-server-3.54.3"
+echo "e42.33.21.evolution-data-server-3.54.3" >> $LFSLOG_PROCESS
+
+time { bash e42.33.21.evolution-data-server-3.54.3.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.22.tinysparql-3.8.2"
+echo "e42.33.22.tinysparql-3.8.2" >> $LFSLOG_PROCESS
+
+time { bash e42.33.22.tinysparql-3.8.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.34.gexiv2-0.14.3"
+echo "e43.33.34.gexiv2-0.14.3" >> $LFSLOG_PROCESS
+
+time { bash e43.33.34.gexiv2-0.14.3.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.23.localsearch-3.8.2"
+echo "e42.33.23.localsearch-3.8.2" >> $LFSLOG_PROCESS
+
+time { bash e42.33.23.localsearch-3.8.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.24.GSound-1.0.3"
+echo "e42.33.24.GSound-1.0.3" >> $LFSLOG_PROCESS
+
+time { bash e42.33.24.GSound-1.0.3.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e42.33.25.xdg-desktop-portal-gnome-47.3"
+echo "e42.33.25.xdg-desktop-portal-gnome-47.3" >> $LFSLOG_PROCESS
+
+time { bash e42.33.25.xdg-desktop-portal-gnome-47.3.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+#####
+
+unset LFSLOG_PROCESS
---
+++ 12.3/e43.Gnome.Desktop.Components/e43.03.sh
@@ -0,0 +1,244 @@
+# e42.03.sh
+#
+# Gnome Desktop Components
+#
+
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+#####
+
+echo "e43.33.31.DConf-0.40.0"
+echo "e43.33.31.DConf-0.40.0" >> $LFSLOG_PROCESS
+
+time { bash e43.33.31.DConf-0.40.0.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.32.gnome-backgrounds-47.0"
+echo "e43.33.32.gnome-backgrounds-47.0" >> $LFSLOG_PROCESS
+
+time { bash e43.33.32.gnome-backgrounds-47.0.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.33.Gvfs-1.56.1"
+echo "e43.33.33.Gvfs-1.56.1" >> $LFSLOG_PROCESS
+
+time { bash e43.33.33.Gvfs-1.56.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+#
+#	build ahead in e42.03.sh:
+#		e43.33.34.gexiv2-0.14.3
+#
+###
+
+echo "e43.33.35.Nautilus-47.2"
+echo "e43.33.35.Nautilus-47.2" >> $LFSLOG_PROCESS
+
+time { bash e43.33.35.Nautilus-47.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+# echo "e43.33.36.gnome-bluetooth-47.1"
+# echo "e43.33.36.gnome-bluetooth-47.1" >> $LFSLOG_PROCESS
+#
+# time { bash e43.33.36.gnome-bluetooth-47.1.sh ; }
+#
+# date +"%Y/%b/%d %H:%M"
+# date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+#
+# echo "." >> $LFSLOG_PROCESS
+# echo "."
+# echo "."
+
+###
+
+echo "e43.33.37.gnome-keyring-46.2"
+echo "e43.33.37.gnome-keyring-46.2" >> $LFSLOG_PROCESS
+
+time { bash e43.33.37.gnome-keyring-46.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.44.gnome-session-47.0.1"
+echo "e43.33.44.gnome-session-47.0.1" >> $LFSLOG_PROCESS
+
+time { bash e43.33.44.gnome-session-47.0.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.38.gnome-settings-daemon-47.2"
+echo "e43.33.38.gnome-settings-daemon-47.2" >> $LFSLOG_PROCESS
+
+time { bash e43.33.38.gnome-settings-daemon-47.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.39.Tecla-47.0"
+echo "e43.33.39.Tecla-47.0" >> $LFSLOG_PROCESS
+
+time { bash e43.33.39.Tecla-47.0.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.40.gnome-control-center-47.4"
+echo "e43.33.40.gnome-control-center-47.4" >> $LFSLOG_PROCESS
+
+time { bash e43.33.40.gnome-control-center-47.4.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.41.Mutter-47.5"
+echo "e43.33.41.Mutter-47.5" >> $LFSLOG_PROCESS
+
+time { bash e43.33.41.Mutter-47.5.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.42.gnome-shell-47.4"
+echo "e43.33.42.gnome-shell-47.4" >> $LFSLOG_PROCESS
+
+time { bash e43.33.42.gnome-shell-47.4.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.43.gnome-shell-extensions-47.4"
+echo "e43.33.43.gnome-shell-extensions-47.4" >> $LFSLOG_PROCESS
+
+time { bash e43.33.43.gnome-shell-extensions-47.4.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+#
+#	build ahead:
+#		e43.33.44.gnome-session-47.0.1
+#
+###
+
+echo "e43.33.45.gnome-tweaks-46.1"
+echo "e43.33.45.gnome-tweaks-46.1" >> $LFSLOG_PROCESS
+
+time { bash e43.33.45.gnome-tweaks-46.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.46.gnome-user-docs-47.2"
+echo "e43.33.46.gnome-user-docs-47.2" >> $LFSLOG_PROCESS
+
+time { bash e43.33.46.gnome-user-docs-47.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "e43.33.47.Yelp-42.2"
+echo "e43.33.47.Yelp-42.2" >> $LFSLOG_PROCESS
+
+time { bash e43.33.47.Yelp-42.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+#####
+
+unset LFSLOG_PROCESS
