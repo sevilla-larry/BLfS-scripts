24b8cbbd828e54fb759bc112ebbfff855da0dedd
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-07-03 15:59:24 +0800

    log bug fix

--- 12.3/b10.common.Xorg.and.servers/b10.13.24.41.build-1.2.2.post1.sh
+++ 12.3/b10.common.Xorg.and.servers/b10.13.24.41.build-1.2.2.post1.sh
@@ -8,27 +8,27 @@
 #
 
 export PKG="build-1.2.2.post1"
-export PKGLOG_DIR=$OSLOG/13.24.41
+export PKGLOG_DIR=$LFSLOG/13.24.41
 export PKGLOG_TAR=$PKGLOG_DIR/tar.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
 export PKGLOG_CHECK=$PKGLOG_DIR/check.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
-export OSLOG_PROCESS=$OSLOG/process.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
 export SOURCES=`pwd`
 
 rm -r $PKGLOG_DIR 2> /dev/null
 mkdir $PKGLOG_DIR
 
 echo "1. Extract tar..."
-echo "1. Extract tar..." >> $OSLOG_PROCESS
+echo "1. Extract tar..." >> $LFSLOG_PROCESS
 echo "1. Extract tar..." >> $PKGLOG_ERROR
 tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
 cd $PKG
 
 
 echo "2. pip3 Build ..."
-echo "2. pip3 Build ..." >> $OSLOG_PROCESS
+echo "2. pip3 Build ..." >> $LFSLOG_PROCESS
 echo "2. pip3 Build ..." >> $PKGLOG_ERROR
 pip3 wheel  -w dist                 \
             --no-cache-dir          \
@@ -38,7 +38,7 @@ pip3 wheel  -w dist                 \
             > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
 
 echo "3. pip3 Install ..."
-echo "3. pip3 Install ..." >> $OSLOG_PROCESS
+echo "3. pip3 Install ..." >> $LFSLOG_PROCESS
 echo "3. pip3 Install ..." >> $PKGLOG_ERROR
 pip3 install    --no-index              \
                 --no-user               \
@@ -48,7 +48,7 @@ pip3 install    --no-index              \
                 > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
 echo "4. pyTest ..."
-echo "4. pyTest ..." >> $OSLOG_PROCESS
+echo "4. pyTest ..." >> $LFSLOG_PROCESS
 echo "4. pyTest ..." >> $PKGLOG_ERROR
 pytest >  $PKGLOG_CHECK 2>> $PKGLOG_ERROR
 
@@ -56,7 +56,7 @@ pytest >  $PKGLOG_CHECK 2>> $PKGLOG_ERROR
 cd $SOURCES
 rm -rf $PKG
 unset SOURCES
-unset OSLOG_PROCESS
+unset LFSLOG_PROCESS
 unset PKGLOG_INSTALL PKGLOG_BUILD
 unset PKGLOG_CHECK
 unset PKGLOG_ERROR PKGLOG_TAR
