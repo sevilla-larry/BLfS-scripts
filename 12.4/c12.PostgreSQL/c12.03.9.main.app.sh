d597eb41c8e62a9880b129eb6c203c7b475a5843
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-09-03 19:37:45 +0800

    Upload PostgreSQL

---
+++ 12.4/c12.PostgreSQL/c12.03.9.main.app.sh
@@ -0,0 +1,27 @@
+# c12.03.9.main.app.sh
+#
+# Databases
+#
+
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+#####
+
+echo "c12.22.04.PostgreSQL-17.6"
+echo "c12.22.04.PostgreSQL-17.6" >> $LFSLOG_PROCESS
+
+time { bash c12.22.04.PostgreSQL-17.6.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+#####
+
+unset LFSLOG_PROCESS
